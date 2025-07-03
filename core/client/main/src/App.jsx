97bad7177be1fe7e9ef0b56dea2232c2a6615715
Adrian Kayllan <adrian.profissional@proton.me>
2025-07-02 21:41:19 -0500

    Compilador de JSX to JS adicionado

--- core/client/main/src/App.jsx
+++ core/client/main/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
   return (
-      <div>
-        <h1>Hello, World!</h1>
+      <div className="container">
+        <h1>Olá, Mundo!</h1>
       </div>
   )
 }
--- core/client/main/vite.config.js
+++ core/client/main/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'C:/Users/PC HOME/Testando-React-no-Django/core/main/static/js',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/main.jsx'),
+      output: {
+        entryFileNames: 'main.js', // <---- Nome fixo!
+        assetFileNames: '[name].[ext]' // evita hashes nos arquivos de estilo ou outros
+      }
+    }
+  }
+});
